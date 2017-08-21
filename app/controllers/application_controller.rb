c368a0bea971213391279b905ea830f59bb53ea9
Kevin <kevingm31@gmail.com>
2017-08-21 10:15:30 -0500

    Add devise securities controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception 
 
   def index
-    render html: "lets test this thing out"
+    render html: "Please sign in"
+    if case_manager_signed_in? 
+      link_to('sign_out', destroy_case_manager_session_path, method: :delete)       
+    else 
+      link_to('sign_in', case_manager_session_path) 
+    end 
   end 
 
 end
